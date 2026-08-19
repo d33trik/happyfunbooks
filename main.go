2dc955c0c9bbbc9bf93e25281bfdf6d8f2aa3a65
Claudio Dietrich <d33trik@gmail.com>
2026-08-19 15:50:24 -0300

    Extract book title to variable
    Move hardcoded book string to a named variable
    for improved readability and future flexibility.

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	book := "'Master and Commander', by Patrick O'Brian"
+
 	fmt.Println("Books in stock:")
-	fmt.Println("'Master and Commander', by Patrick O'Brian")
+	fmt.Println(book)
 }
