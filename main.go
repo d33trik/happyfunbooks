1a4636d10b3a4712fb237d1068363ad45c43ea34
Claudio Dietrich <d33trik@gmail.com>
2026-08-19 17:02:38 -0300

    Extract book printing to helper func
    Deduplicate the repeated Println call into a
    printBook function to reduce code duplication

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ func main() {
 	fmt.Println("Books in stock:")
 
 	title, author := "Master and Commander", "Patrick O'Brian"
-	fmt.Println(title, "by", author)
+	printBook(title, author)
 
 	title, author = "A Morbid Taste for Bones", "Ellis Peters"
+	printBook(title, author)
+}
+
+func printBook(title, author string) {
 	fmt.Println(title, "by", author)
 }
