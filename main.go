65fbb1154e08da34f4ffdf64c1879da415e97c23
Claudio Dietrich <d33trik@gmail.com>
2026-08-19 16:50:01 -0300

    Add second book to listing
    Include a second book alongside the existing one
    to expand the book inventory display.

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	book := "'Master and Commander', by Patrick O'Brian"
-
 	fmt.Println("Books in stock:")
+
+	book := "'Master and Commander', by Patrick O'Brian"
+	fmt.Println(book)
+
+	book = "'A Morbid Taste for Bones', by Ellis Peters"
 	fmt.Println(book)
 }
