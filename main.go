729b66780227b705a0f786846776a1868be99066
Claudio Dietrich <d33trik@gmail.com>
2026-08-24 13:13:03 -0300

    Move BookToString test to main_test.go
    Replace the hand-rolled test function called from main() with an
    idiomatic test file using the standard testing package, runnable
    via go test.

--- main.go
+++ main.go
@@ -12,23 +12,7 @@ func BookToString(b Book) string {
 	return fmt.Sprintf("%v, by %v - %v copies", b.Title, b.Author, b.Copies)
 }
 
-func TestBookToString_FormatsBookInfoAsString() {
-	input := Book{
-		Title:  "Sea Room",
-		Author: "Adam Nicolson",
-		Copies: 2,
-	}
-
-	want := "Sea Room, by Adam Nicolson - 2 copies"
-	got := BookToString(input)
-	if want != got {
-		panic("BookToString: wrong result")
-	}
-}
-
 func main() {
-	TestBookToString_FormatsBookInfoAsString()
-
 	fmt.Println("Books in stock:")
 
 	book := Book{
