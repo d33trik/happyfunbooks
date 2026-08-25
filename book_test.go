e696153d68a0db670387897d6487477ee6a2cd32
Claudio Dietrich <d33trik@gmail.com>
2026-08-25 15:07:58 -0300

    Add book catalog and GetAllBooks function
    Add hardcoded catalog of two books and a function to retrieve
    them, along with tests.

--- book_test.go
+++ book_test.go
@@ -1,6 +1,7 @@
 package happyfunbooks_test
 
 import (
+	"slices"
 	"testing"
 
 	"github.com/d33trik/happyfunbooks"
@@ -17,5 +18,24 @@ func TestBookToString_FormatsBookInfoAsString(t *testing.T) {
 	got := happyfunbooks.BookToString(input)
 	if want != got {
 		t.Fatalf("want: %q, got: %q", want, got)
+	}
+}
+
+func TestGetAllBooks_RetrunsAllBooks(t *testing.T) {
+	want := []happyfunbooks.Book{
+		{
+			Title:  "In the Company of Cheerful Ladies",
+			Author: "Alexander McCall Smith",
+			Copies: 1,
+		},
+		{
+			Title:  "White Heat",
+			Author: "Dominic Sandbrook",
+			Copies: 2,
+		},
+	}
+	got := happyfunbooks.GetAllBooks()
+	if !slices.Equal(want, got) {
+		t.Fatalf("want: %#v, got: %#v", want, got)
 	}
 }
