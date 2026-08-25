f7974fd56bea81ccf0f0af46a63f2c9cbaf471d5
Claudio Dietrich <d33trik@gmail.com>
2026-08-25 15:09:02 -0300

    Use catalog to list books in cmd/list

--- cmd/list/main.go
+++ cmd/list/main.go
@@ -9,17 +9,7 @@ import (
 func main() {
 	fmt.Println("Books in stock:")
 
-	book := happyfunbooks.Book{
-		Title:  "Master and Commander",
-		Author: "Patrick O'Brian",
-		Copies: 17,
+	for _, book := range happyfunbooks.GetAllBooks() {
+		fmt.Println(happyfunbooks.BookToString(book))
 	}
-	fmt.Println(happyfunbooks.BookToString(book))
-
-	book = happyfunbooks.Book{
-		Title:  "A Morbid Taste for Bones",
-		Author: "Ellis Peters",
-		Copies: 42,
-	}
-	fmt.Println(happyfunbooks.BookToString(book))
 }
